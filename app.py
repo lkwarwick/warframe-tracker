437a8d2942d36d24fec01a8e953fac42acb38e17
lkwarwick <lucaswarwick02@gmail.com>
2026-06-16 08:34:31 +0100

    feat: add template dash app + pydantic model combination logic

---
+++ app.py
@@ -0,0 +1,15 @@
+# /// script
+# dependencies = [
+#   "dash",
+# ]
+# ///
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello World!')
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
