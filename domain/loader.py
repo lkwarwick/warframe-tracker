7705da7c4ed1b243936be8273794aa69e30fe3be
lkwarwick <lucaswarwick02@gmail.com>
2026-06-18 21:42:46 +0100

    feat: remove helminth from warframes

--- domain/loader.py
+++ domain/loader.py
@@ -11,7 +11,7 @@ def load_warframes() -> list[Warframe]:
     logger.info("Loading warframes from remote source...")
     raw = requests.get(WF_URL, timeout=10).json()
     logger.info(f"Loaded {len(raw)} warframes from remote source.")
-    return [Warframe.model_validate(x) for x in raw]
+    return [Warframe.model_validate(x) for x in raw if not x.get("uniqueName") == "/Lotus/Powersuits/PowersuitAbilities/Helminth"]
 
 def load_primaries() -> list[Warframe]:
     logger.info("Loading primaries from remote source...")
